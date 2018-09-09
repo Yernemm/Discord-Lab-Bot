c39f51a4777c7ec5578e50d283fb761b832d9da3
Yernemm <yernemm@yernemm.xyz>
2018-09-09 02:56:37 +0100

    added crypto cmd with shift

---
+++ commands/crypto.js
@@ -0,0 +1,69 @@
+//METADATA
+const desc = "Cryptography and base shifts"; //Short description of what the command does.
+const usage = ""; //Any parameters required for command.
+const cmdtype = "utility"; //Type of command
+//Command
+exports.run = (config, client, message, argsArr, argsTxt, extraData) => {
+    const bot = client;
+    const m = require("./../shared/methods.js");
+    const alphaCap = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const alpha = "abcdefghijklmnopqrstuvwxyz";
+    //--------------------------------------------------------------------
+    //Uncomment for protected owner-only command.
+    //if(message.author.id !== config.ownerID) return; 
+    //--------------------------------------------------------------------
+    var msg = ""; //Put response message to be logged here.
+    //--------------------------------------------------------------------
+
+    //COMMAND LOGIC HERE:
+
+    switch (argsArr[0]){
+        case "shift":
+        case "rot":
+        case "caesar":
+        if (Number.isInteger(argsArr[1]))
+        {
+            var shift = argsArr[1];
+            var msgBefore = argsTxt.substring(argsArr[0] + argsArr[1] + 2);
+            var msgNew = "";
+            msgBefore.forEach(c =>{
+                var upper = (c == c.toUpperCase());
+                var pos = alpha.indexOf(c.toLowerCase());
+                if (pos == -1){
+                    msgNew += c;
+                }else{
+                    pos = (pos + shift) % alpha.length;
+                    var newChar = alpha[pos];
+                    if(upper)
+                    newChar = newChar.toLocaleUpperCase();
+                    msgNew += newChar;
+                }
+            });
+            msg = msgNew;
+
+        }else
+        {
+            msg="Error: Shift is not an integer."
+        }
+
+        break;
+        default:
+        msg = "Error: Invalid crypto mode. Use 'help crypto' for more information."
+        break;
+    }
+
+
+
+    //--------------------------------------------------------------------
+    m.logSend(config, client, message, msg); //Method will send msg to user, and also log it in both console AND log channel.
+    //m.log(config, client, message, msg); //Alternative will log msg without sending msg.
+}
+exports.desc = () =>{
+    return desc;
+}
+exports.use = () =>{
+    return usage;
+}
+exports.cmdtype = () => {
+    return cmdtype;
+}
